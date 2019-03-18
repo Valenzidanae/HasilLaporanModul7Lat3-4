6f1fb7184b749e180955969082658bce0e6aa60c
Valenzidanae <46430109+Valenzidanae@users.noreply.github.com>
2019-03-18 09:50:38 +0700

    Add files via upload

---
+++ form-login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+    <form action="login.php" method="POST">
+    <table>
+    <tr>
+        <td><label>username</label></td>
+        <td>:</td>
+        <td><input type="text" name="username"></td>
+    </tr>
+    <tr>
+        <td><label>password</label></td>
+        <td>:</td>
+        <td><input type="password" name="password"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td><input type="submit" value="simpan"></td>
+    </tr>
+    </table>
+    </form>
+</body>
+</html>
